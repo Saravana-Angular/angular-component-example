d6746237e1c7a8fab87bca17774bd5e72ad6ecfa
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-05-12 10:13:03 -0400

    styled warning-alert using inline styles

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -3,5 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-warning-alert",
   template: `<p>This is a warning message</p>`,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: yellow;
+        border: 5px solid red;
+      }
+    `,
+  ],
 })
 export class WarningAlert {}
